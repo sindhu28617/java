4553f561b751e02af8f12e2e49823b7e64a9154f
sindhu28617 <sindhudhana56@gmail.com>
2024-11-19 17:55:58 +0530

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,21 +1,19 @@
 package AllProgram
 {
 		public class AddNumbers {
-
-		    // This method takes two integers as input and returns their sum
+			
 		    public static int add(int number1, int number2) {
-		        int sum = number1 + number2; // Calculate the sum
-		        return sum; // Return the sum to where the method was called
+		        int sum = number1 + number2; 
+		        return sum; 
 		    }
 
 		    public static void main(String[] args) {
 		        int num1 = 4; // First number
 		        int num2 = 8; // Second number
 
-		        // Call the add method and store the result in total
+		        
 		        int total = add(num1, num2);
 
-		        // Display the result
 		        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + total);
 		    }
 	}

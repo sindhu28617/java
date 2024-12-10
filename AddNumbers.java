1938afbd21affead65c8ac8f0aeb306104b18d1d
sindhu28617 <sindhudhana56@gmail.com>
2024-12-10 18:20:44 +0530

    Add files via upload

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,19 +1,22 @@
 package AllProgram
-{
+
 		public class AddNumbers {
-			
-		    public static int add(int number1, int number2) {
-		        int sum = number1 + number2; 
-		        return sum; 
-		    }
 
 		    public static void main(String[] args) {
 		        int num1 = 4; // First number
 		        int num2 = 8; // Second number
-		        
+
+		        // Call the add method and store the result in total
 		        int total = add(num1, num2);
 
+		        // Display the result
 		        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + total);
 		    }
+
+			// This method takes two integers as input and returns their sum
+			public static int add(int number1, int number2) {
+			    int sum = number1 + number2; // Calculate the sum
+			    return sum; // Return the sum to where the method was called
+			}
 	}
 
